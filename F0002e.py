da2732b0b54f473ed9554ef12a1f16c55ee579e3
kalmanlaszlo06 <71382602+kalmanlaszlo06@users.noreply.github.com>
2020-10-14 14:44:24 +0200

    Add files via upload

---
+++ F0002e.py
@@ -0,0 +1,3 @@
+#F0002e: Ha visszapert (backslash, “\”) akarunk kiírni a print()-tel, akkor az utasításban két visszapert kell írnunk, mert az első arra jó, hogy levédje az utána álló karaktert, mint a \n esetén már láttad. Ennek fényében: Rajzold ki egyetlen print() utasítással az alábbi ábrát!
+
+print('\\\n \\\n  \\\n   \\\n    \\\n     \\\n')
